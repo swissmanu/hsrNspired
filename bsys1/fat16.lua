d564b95c54c324e7ba935bb95532af4c4b6b3c9d
Manuel Alabor <msites@msites.net>
2012-02-01 21:29:23 +0100

    namespace completly implemented

--- bsys1/fat16.lua
+++ bsys1/fat16.lua
@@ -1,17 +1,9 @@
-__fat16 = {
-}
+__fat16 = {}
 
-FAT_ENTRY = ''
 toolpalette.register({})
 toolpalette.enablePaste(true)
 
-
-
-
-
-
-
-
+__fat16.input_hex_value = ''
 
 -- FUNCTIONS -------------------------------------------------------------
 
@@ -32,7 +24,7 @@ end
 
 -- Reverses Hex bytes in a string
 function __fat16.reverseHexString(hex_string)
-	local bytes = splitHexBytes(hex_string)
+	local bytes = __fat16.splitHexBytes(hex_string)
 	local reversed = ''
 	
 	for i=table.getn(bytes), 1, -1 do
@@ -44,11 +36,11 @@ end
 
 -- Converts a string with hex bytes into its ascii representation
 function __fat16.toAscii(hex_input)
-	local bytes = splitHexBytes(hex_input)
+	local bytes = __fat16.splitHexBytes(hex_input)
 	local text = ''
 	
 	for i=1,table.getn(bytes) do
-		text = text .. string.char(tonumber(prefixHex(bytes[i])))
+		text = text .. string.char(tonumber(__fat16.prefixHex(bytes[i])))
 	end
 	
 	return text
@@ -111,37 +103,36 @@ end
 
 -- OUTPUT CONVERTERS -----------------------------------------------------
 function __fat16.convertFilename(hex)
-	return toAscii(hex)
+	return __fat16.toAscii(hex)
 end
 
 function __fat16.convertExtension(hex)
-	return toAscii(hex)
+	return __fat16.toAscii(hex)
 end
 
 function __fat16.convertAttribute(hex)
-	local hex = prefixHex(hex)
-	local bits = toBits(tonumber(hex))
+	local hex = __fat16.prefixHex(hex)
+	local bits = __fat16.toBits(tonumber(hex))
 	local attributes = ''
 	
 	for i=1,table.getn(bits), 1 do
 		if bits[i] == 1 then
-			if i == 1 then attributes = attributes..'r(ead only) '
-			elseif i == 2 then attributes = attributes..'h(idden) '
-			elseif i == 3 then attributes = attributes..'s(ystem) '
-			elseif i == 4 then attributes = attributes..'v(olume) '
-			elseif i == 5 then attributes = attributes..'d(irectory) '
-			elseif i == 6 then attributes = attributes..'a(rchive) '
+			if i == 1 then attributes = attributes..'r '
+			elseif i == 2 then attributes = attributes..'h '
+			elseif i == 3 then attributes = attributes..'s '
+			elseif i == 4 then attributes = attributes..'v '
+			elseif i == 5 then attributes = attributes..'d '
+			elseif i == 6 then attributes = attributes..'a '
 			end
 		end
 	end
-	
 	
 	return attributes
 end
 
 function __fat16.convertTime(hex)
-	local hex = reverseHexString(hex)
-	local bits = toBits(tonumber(prefixHex(hex)),16)
+	local hex = __fat16.reverseHexString(hex)
+	local bits = __fat16.toBits(tonumber(__fat16.prefixHex(hex)),16)
 	local hourBits = ''
 	local minuteBits = ''
 	local secondBits = ''
@@ -153,15 +144,15 @@ function __fat16.convertTime(hex)
 		end
 	end
 	
-	local time = bitStringToDec(hourBits)..':'..
-				 bitStringToDec(minuteBits)..':'..
-				 bitStringToDec(secondBits)*2
+	local time = __fat16.bitStringToDec(hourBits)..':'..
+				 __fat16.bitStringToDec(minuteBits)..':'..
+				 __fat16.bitStringToDec(secondBits)*2
 	return time
 end
 
 function __fat16.convertDate(hex)
-	local hex = reverseHexString(hex)
-	local bits = toBits(tonumber(prefixHex(hex)),16)
+	local hex = __fat16.reverseHexString(hex)
+	local bits = __fat16.toBits(tonumber(__fat16.prefixHex(hex)),16)
 	local dayBits = ''
 	local monthBits = ''
 	local yearBits = ''
@@ -173,89 +164,89 @@ function __fat16.convertDate(hex)
 		end
 	end
 	
-	local date = bitStringToDec(dayBits)..'.'..
-				 bitStringToDec(monthBits)..'.'..
-				 1980+bitStringToDec(yearBits)
+	local date = __fat16.bitStringToDec(dayBits)..'.'..
+				 __fat16.bitStringToDec(monthBits)..'.'..
+				 __fat16.bitStringToDec(yearBits)+1980
 	return date
 end
 
 function __fat16.convertCluster(hex)
-	local reversed = prefixHex(reverseHexString(hex))
+	local reversed = __fat16.prefixHex(__fat16.reverseHexString(hex))
 	return reversed..', '..tonumber(reversed)
 end
 
 function __fat16.convertFilesize(hex)
-	local reversed = prefixHex(reverseHexString(hex))
+	local reversed = __fat16.prefixHex(__fat16.reverseHexString(hex))
 	return reversed..', '..tonumber(reversed).. 'byte'
 end
 
 -- HANDLERS --------------------------------------------------------------
 function __fat16.handleFieldValue(index, value)
-	if index	 == 1 then 	return 'Filename: '..convertFilename(value)
-	elseif index == 2 then	return 'Extension: '..convertExtension(value)
-	elseif index == 3 then	return 'Attribute: '..convertAttribute(value)
+	if index	 == 1 then 	return 'Filename: '..__fat16.convertFilename(value)
+	elseif index == 2 then	return 'Extension: '..__fat16.convertExtension(value)
+	elseif index == 3 then	return 'Attribute: '..__fat16.convertAttribute(value)
 	--elseif index == 4 then  reserved ;-)
-	elseif index == 5 then	return 'Time last changed: '..convertTime(value)
-	elseif index == 6 then	return 'Date last changed: '..convertDate(value)
-	elseif index == 7 then	return 'First cluster: '..convertCluster(value)
-	elseif index == 8 then  return 'Actual size: '..convertFilesize(value)
+	elseif index == 5 then	return 'Time last changed: '..__fat16.convertTime(value)
+	elseif index == 6 then	return 'Date last changed: '..__fat16.convertDate(value)
+	elseif index == 7 then	return 'First cluster: '..__fat16.convertCluster(value)
+	elseif index == 8 then  return 'Actual size: '..__fat16.convertFilesize(value)
 	end
 	
 	return ""
+end
+
+function __fat16.drawOutput(gc, input)
+	gc:setFont('sansserif','r',10)
+
+	-- FAT16 Entry Definition:
+	local entry_byte_length = 32
+	local field_lengths = {
+		8		-- Filename
+		,3		-- Extension
+		,1		-- Attribute
+		,10		-- Reserved
+		,2		-- Time last changed
+		,2		-- Date last changed
+		,2		-- First cluster
+		,4		-- Filesize in bytes
+	}
+
+	--======================================================================--
+	-- MAIN
+	--======================================================================--
+	--input = '47494D504F52542048202020184DF8A9383E383E0000D18C683C0400AD200000'
+	if string.len(input) ~= entry_byte_length*2 then
+		gc:setColorRGB(255,0,0)
+		gc:drawString("Please paste a value with "..entry_byte_length.." hex bytes!",10,10,"top")
+	else
+		-- CALCULATE FIELD OFFSETS -----------------------------------------------
+		field_offsets = __fat16.calculateFieldOffsets(field_lengths)
+
+		-- PROCESS FIELDS --------------------------------------------------------
+		for i=1, table.getn(field_lengths) do
+			offset = field_offsets[i]*2
+			length = field_lengths[i]*2
+			field_value = string.sub(input, offset+1, offset+length)
+
+			processed_value = __fat16.handleFieldValue(i, field_value)
+			if processed_value ~= "" then
+				gc:drawString(processed_value,10,10+18*(i-1),"top")
+			end
+		end
+	end
 end
 
 
 function __fat16.main()
 	
 	on.paste = function()
-		FAT_ENTRY = clipboard.getText()
+		__fat16.input_hex_value = clipboard.getText()
 		platform.window:invalidate()
 	end
 
-	function drawOutput(gc, input)
-		gc:setFont('sansserif','r',10)
-	
-		-- FAT16 Entry Definition:
-		local entry_byte_length = 32
-		local field_lengths = {
-			8		-- Filename
-			,3		-- Extension
-			,1		-- Attribute
-			,10		-- Reserved
-			,2		-- Time last changed
-			,2		-- Date last changed
-			,2		-- First cluster
-			,4		-- Filesize in bytes
-		}
-
-		--======================================================================--
-		-- MAIN
-		--======================================================================--
-		--input = '47494D504F52542048202020184DF8A9383E383E0000D18C683C0400AD200000'
-		if string.len(input) ~= entry_byte_length*2 then
-			gc:setColorRGB(255,0,0)
-			gc:drawString("Please paste a value with "..entry_byte_length.." hex bytes!",10,10,"top")
-		else
-			-- CALCULATE FIELD OFFSETS -----------------------------------------------
-			field_offsets = calculateFieldOffsets(field_lengths)
-
-			-- PROCESS FIELDS --------------------------------------------------------
-			for i=1, table.getn(field_lengths) do
-				offset = field_offsets[i]*2
-				length = field_lengths[i]*2
-				field_value = string.sub(input, offset+1, offset+length)
-
-				processed_value = handleFieldValue(i, field_value)
-				if processed_value ~= "" then
-					gc:drawString(processed_value,10,10+18*(i-1),"top")
-				end
-			end
-		end
-	end
-
-	function on.paint(gc)
-		if FAT_ENTRY ~= '' then
-			drawOutput(gc, FAT_ENTRY)
+	on.paint = function(gc)
+		if __fat16.input_hex_value ~= '' then
+			__fat16.drawOutput(gc, __fat16.input_hex_value)
 		else
 			gc:setFont('sansserif','r',10)
 			gc:setColorRGB(255,0,0)
